9287b80102a6fa9aa9e9ebfb2cd93c2497a16c31
zhang <741549558@qq.com>
2017-10-15 18:54:44 +0800

    run lda

--- src/lda/lda.py
+++ src/lda/lda.py
@@ -1,26 +1,28 @@
 # coding=utf-8
 import sys
 import random
+import cPickle
 from gensim import corpora, models
-import src.util as util
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 if __name__ == "__main__":
-    corpus = util.load_cPickle('data_for_lda.p')
-    lda = models.ldamodel.LdaModel(corpus, num_topics=4, alpha='symmetric', eval_every=5, iterations=1000,
+    corpus = cPickle.load(open('/home/zhang/Documents/data_file_2017/data_for_lda.p', "rb"))
+    lda = models.ldamodel.LdaModel(corpus, num_topics=100, alpha='symmetric', eval_every=5, iterations=10,
                                    gamma_threshold=0.001, minimum_probability=0.01, random_state=1)
 
-    doc_topic = []
-    for index in range(len(corpus)):
-        max_p = 0.
-        topic = ''
-        for ti in lda[corpus[index]]:
-            if ti[1] > max_p:
-                topic = ti[0]
-                max_p = ti[1]
-        doc_topic.append(topic)
+    lda.save('/home/zhang/Documents/data_file_2017/lda.model')
 
-    for i in range(30):
-        print str(i + 1) + ":" + str(doc_topic[i]) + "->" + str(doc_topic[i + 30])
+    # doc_topic = []
+    # for index in range(len(corpus)):
+    #     max_p = 0.
+    #     topic = ''
+    #     for ti in lda[corpus[index]]:
+    #         if ti[1] > max_p:
+    #             topic = ti[0]
+    #             max_p = ti[1]
+    #     doc_topic.append(topic)
+    #
+    # for i in range(30):
+    #     print str(i + 1) + ":" + str(doc_topic[i]) + "->" + str(doc_topic[i + 30])
